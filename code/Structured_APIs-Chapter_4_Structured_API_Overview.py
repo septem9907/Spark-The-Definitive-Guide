0a42f4863c0b3a5db9e567ed88ebf7c19eceefd6
septem9907 <septem9907@gmail.com>
2020-05-11 16:41:37 +0800

    update chapter 4 & 5

--- code/Structured_APIs-Chapter_4_Structured_API_Overview.py
+++ code/Structured_APIs-Chapter_4_Structured_API_Overview.py
@@ -1,17 +1,18 @@
+# %%
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("spark-book").getOrCreate()
+
+spark
+
+# %%
 df = spark.range(500).toDF("number")
 df.select(df["number"] + 10)
 
-
-# COMMAND ----------
-
+# %%
 spark.range(2).collect()
 
-
-# COMMAND ----------
-
+# %%
 from pyspark.sql.types import *
 b = ByteType()
 
-
-# COMMAND ----------
-
+# %%
