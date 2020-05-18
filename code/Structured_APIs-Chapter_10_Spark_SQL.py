637ffde0363d5eac598e982bb42c906c862ac7c3
septem9907 <septem9907@gmail.com>
2020-05-18 11:38:00 +0800

    updated chapter 8 - 10

--- code/Structured_APIs-Chapter_10_Spark_SQL.py
+++ code/Structured_APIs-Chapter_10_Spark_SQL.py
@@ -1,13 +1,19 @@
-spark.read.json("/data/flight-data/json/2015-summary.json")\
-  .createOrReplaceTempView("some_sql_view") # DF => SQL
+# %%
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("spark-book").getOrCreate()
+
+# %%
+spark
+
+# %%
+spark.read.json("../data/flight-data/json/2015-summary.json")\
+    .createOrReplaceTempView("some_sql_view") # DF => SQL
 
 spark.sql("""
 SELECT DEST_COUNTRY_NAME, sum(count)
 FROM some_sql_view GROUP BY DEST_COUNTRY_NAME
 """)\
-  .where("DEST_COUNTRY_NAME like 'S%'").where("`sum(count)` > 10")\
-  .count() # SQL => DF
+    .where("DEST_COUNTRY_NAME like 'S%'").where("`sum(count)` > 10")\
+    .count() # SQL => DF
 
-
-# COMMAND ----------
-
+# %%
